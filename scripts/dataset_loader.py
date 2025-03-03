5943ef0bf3181e5ece5d9d2e7bc2be9a431584d1
Mann Dharmesh Acharya <mann.compi@gmail.com>
2025-03-03 23:00:27 +0530

    adding batch size to dataset loader

--- scripts/dataset_loader.py
+++ scripts/dataset_loader.py
@@ -2,13 +2,15 @@ import numpy as np
 import tensorflow as tf
 
 
-def load_dataset():
-
+def load_dataset(batch_size=32):
     images = np.load("data/images.npy")
     labels = np.load("data/labels.npy")
     images = images.astype("float32")
+
     dataset = tf.data.Dataset.from_tensor_slices((images, labels))
-    return dataset, images, labels
+    # Adding Batch Size
+    dataset = dataset.batch(batch_size)
+    return dataset
 
 
 if __name__ == "__main__":
